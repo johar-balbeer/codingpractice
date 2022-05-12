a26e42f17a0c8d32ddb57741441dc2ff412873e9
Balbeer Johar <balbeerjohar@Balbeers-MacBook-Pro.local>
2022-05-12 00:02:23 -0400

    chnages

--- ds-programs/src/com/ds/practice/LongestCommonPrefix.java
+++ ds-programs/src/com/ds/practice/LongestCommonPrefix.java
@@ -1,36 +1,31 @@
 package com.ds.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class LongestCommonPrefix {
 
-    static String commonPrefix(String input1,String input2) {
-        String result = "";
-        for (int i = 0, j = 0; i < input1.length() && j < input2.length(); i++, j++) {
-            if (input1.charAt(i) != input2.charAt(j)) {
-                break;
-            }
-            result += input1.charAt(i);
-        }
-        return result;
-    }
-
-    static String searchCommonPrefix(List<String> inputLst) {
-        String prefix = inputLst.get(0);
-        for (int i = 1; i < inputLst.size(); i++) {
-            prefix = commonPrefix(prefix, inputLst.get(i));
-        }
-        return prefix;
-    }
-
-
-
     public static void main(String[] args) {
-        List inputStringLst=Arrays.asList("Bun","Burger","Bummer");
-        String result=searchCommonPrefix(inputStringLst);
-        System.out.println("LCP is: "+result);
+        List<String> inputStringLst = new ArrayList<String>();
+        inputStringLst.add("abc");
+        inputStringLst.add("abef");
+        inputStringLst.add("abccc");
+        inputStringLst.add("abftg");
 
+        String commonPrefix = inputStringLst.get(0);
+
+        for (int i = 1; i < inputStringLst.size(); i++) {
+
+            while (inputStringLst.get(i).indexOf(commonPrefix) != 0) {
+
+                commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+
+                if (commonPrefix.isEmpty()) {
+                    System.out.println("No Common prefix");
+                }
+            }
+        }
+        System.out.println(commonPrefix);
     }
-
 }
